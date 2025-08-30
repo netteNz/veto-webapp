39f2b09d9dac4b2a90a4c00c944a176e9c84a13a
Emanuel Lugo <lugo.emanuel@gmail.com>
2025-08-30 05:58:19 -0400

    #

--- server/api/settings.py
+++ server/api/settings.py
@@ -90,6 +90,8 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+
 _db_url = os.getenv("DATABASE_URL", "").strip()
 
 def sqlite_default():
